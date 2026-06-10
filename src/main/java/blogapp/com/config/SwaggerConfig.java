b39f626b11f23bc949b0218880e438d3b6385658
Sohail <sohailkarmani16@gmail.com>
2026-06-11 00:09:22 +0500

    added

---
+++ src/main/java/blogapp/com/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package blogapp.com.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Blog Application API",
+                "API documentation for the Blog Application",
+                "4.0",
+                "Terms of service URL",
+                new Contact(
+                        "Sohail",
+                        "https://github.com/Sohail-Meghwar/springboot-blog-api",
+                        "sohailkarmani16@gmail.com"
+                ),
+                "License",
+                "License URL",
+                Collections.emptyList()
+        );
+    }
+}
